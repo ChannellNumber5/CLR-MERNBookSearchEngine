d5277642c3e25ef776dc4f51c8692abdd5657850
Christina R <xsnachannell@gmail.com>
2022-07-27 14:16:32 -0700

    updates mutations.js with the post, put and delete mutations from the api.js routes needed for application to run

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,3 +1,45 @@
 import {gql} from '@apollo/client';
 
-export const
+export const CREATE_USER = gql `
+    mutation createUser($username: String!, $email: String!, $password: String!){
+        createUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const LOGIN_USER = gql `
+    mutation login($email: String!, $password: String! ) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql `
+    mutation saveBook($profileId: ID!, $book: Book) {
+        saveBook(profileId: $profileId, book: $book) {
+            _id
+            username
+            savedBooks
+        }
+    }
+`;
+
+export const DELETE_BOOK = gql `
+    mutation deleteBook($book: Book) {
+        deleteBook(book: $book) {
+            _id
+            username
+            savedBooks
+        }
+    }
+`;
