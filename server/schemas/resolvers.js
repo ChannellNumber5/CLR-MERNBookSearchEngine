b5cb42cbac10eaa2a06d7a8085e3a00518c8f892
Christina R <xsnachannell@gmail.com>
2022-07-26 18:54:56 -0700

    updates resolver.js file to include queries and mutations. still need to update them so that user can save books and delete them from their saved book list

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,11 +4,43 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        user: async (parent, {userId}) => {
+            return User.findOne({_id: userId});
+        }
 
     },
 
     Mutation: {
+        createUser: async (parent, {username, email, password}) => {
+            const user = await User.create({username, email, password});
+            const token = signToken(User);
 
+            return {token, user}
+        },
+        login: async (parent, {email, password}) => {
+            const user = await user.findOne({ email });
+
+            if ( !user) {
+                throw new AuthenticationError('No user found associated with this email address');
+            }
+
+            const validPass = await user.isCorrectPassword(password);
+
+            if (!validPass) {
+                throw new AuthenticationError('Incorrect Password');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, { userId, bookData }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    {_id: user._id}
+                )
+            }
+        }
     }, 
 };
 
